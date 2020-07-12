cbca2facb9739eb35d2f13957032aae63fafb8d5
Pascal Martin <pascal.fb.martin@gmail.com>
2020-07-12 16:25:27 -0700

    Add newly separated parser definitions

---
+++ echttp_parser.h
@@ -0,0 +1,36 @@
+/* echttp - Embedded HTTP server.
+ *
+ * A minimal HTTP server library designed for simplicity and embedding in
+ * existing applications.
+ *
+ * echttp_json.h - An additional module to decode JSON text,
+ */
+
+typedef struct {
+    const char *key;
+    int type;
+    union {
+        int bool;
+        long integer;
+        double real;
+        char *string;
+    } value;
+    int length;
+} ParserToken;
+
+// Basic types:
+#define PARSER_NULL    1
+#define PARSER_BOOL    2
+#define PARSER_INTEGER 3
+#define PARSER_REAL    4
+#define PARSER_STRING  5
+#define PARSER_ARRAY   6
+#define PARSER_OBJECT  7
+
+// Code generation options:
+
+#define PRINT_OPTION_PRETTY   1
+
+struct ParserContext_s;
+typedef struct ParserContext_s *ParserContext;
+
