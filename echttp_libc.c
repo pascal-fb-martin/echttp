c480a438afb7e512ec80df6712896202aa688e8f
Pascal Martin <pascal.fb.martin@gmail.com>
2026-04-04 10:58:25 -0700

    Add more protection to stpecpy()

--- echttp_libc.c
+++ echttp_libc.c
@@ -11,6 +11,11 @@
  *
  *    Provide a safe and efficient function for string copy.
  *    See man stpecpy for more information.
+ *
+ *    This implementation tries to conform to all known variants, like how
+ *    to signal truncation, in the safest possible manner. It also adds
+ *    a non-standard protection against null source, because this is common
+ *    with echttp (like missing optional HTTP argument or attribute).
  */
 
 #include <string.h>
@@ -20,7 +25,13 @@
 char *stpecpy (char *dst, char *end, const char *restrict src) {
 
    if (dst == 0) return 0; // Protect against some variant implementations.
-   if (dst == end) return end;
+   if (dst >= end) return end;
+
+   // There is room for at least one byte..
+   if (src == 0) { // Add some custom protection.
+      dst[0] = 0;
+      return dst; // Nothing copied.
+   }
    char *p = memccpy(dst, src, '\0', end - dst);
    if (p) return p - 1;
 
